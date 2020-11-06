b15a7977f91618aa321f72c125da26eb056e0bea
AnaLChavez <50453459+AnaLChavez@users.noreply.github.com>
2020-11-06 11:48:39 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,56 @@
+import React from 'react';
+//import { StyleSheet, Text, View } from 'react-native'; 
+//import { Container, Header, Content, Card, CardItem, Body, Button,Item, Label, Input, Icon} from "native-base";
+import Login from './screem/Login';
+
+const App: () => React$Node =()=>{
+  return (
+    <Login></Login>
+
+    /*<Container>
+        <Header />
+        <Content padder contentContainerStyle = {misEstilos.content}>
+          <Card>
+            <CardItem header bordered>
+             <Text style= {misEstilos.textCenter}>Inicio de sesión</Text>
+            </CardItem>
+            <CardItem bordered>
+              <Body style = {misEstilos.body}>
+                <Item inlineLabel>
+                <Icon type= 'FontAwesome' name= 'user'></Icon>
+                  <Input placeholder= 'Nombre de usuraio'/>
+                </Item>
+                <Item inlineLabel last>
+                <Icon type= 'FontAwesome' name= 'lock'></Icon>
+                  <Input placeholder= 'Contraseña'/>
+                </Item>
+              </Body>
+            </CardItem>
+            <CardItem footer bordered>
+              <Button primary style ={misEstilos.button}><Text> Ingresar</Text></Button>
+            </CardItem>
+          </Card>
+        </Content>
+      </Container>*/
+
+
+
+  );
+};
+/*const misEstilos = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  textCenter:{
+    textAlign: 'center',
+    width: '100%',
+  },
+  button: {
+    marginLeft: '35%',
+  },
+  body: {
+    paddingVertical: 35,
+  }
+});*/
+export default App;
